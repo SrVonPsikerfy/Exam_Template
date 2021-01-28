f5e0deffd9626389ff75f61e673dd04b865100b2
AaronNauzetMorenoSosa <aarmor01@ucm.es>
2021-01-28 22:30:42 +0100

    Main class y phaser de internet

---
+++ template/main.js
@@ -0,0 +1,22 @@
+import Scene from './scene.js'
+import End from './end.js'
+import Boot from './boot.js'
+
+new Phaser.Game({
+    type: Phaser.AUTO,
+    width: 1000,
+    height: 500,
+    scale: {
+        mode: Phaser.Scale.FIT,  
+        autoCenter: Phaser.Scale.CENTER_HORIZONTALLY
+    },
+    pixelArt: true,
+    scene: [Boot, Scene, End],
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 400 },
+            debug: false
+        }
+    }
+});
