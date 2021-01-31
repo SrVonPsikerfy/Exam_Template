fdc1fdad7d48fb3ef198e39cd4952204237503c8
AaronNauzetMorenoSosa <aarmor01@ucm.es>
2021-01-31 22:09:28 +0100

    Added placeholder tilemap and tileset

---
+++ assets/tilemaps/tileset_bg.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="tileset_bg" tilewidth="64" tileheight="64" tilecount="1288" columns="56">
+ <image source="../../../PVLI/assets/sprites/tilesets/slates_tileset.png" width="3584" height="1472"/>
+ <tile id="113">
+  <properties>
+   <property name="obstacle" type="bool" value="false"/>
+  </properties>
+ </tile>
+</tileset>
